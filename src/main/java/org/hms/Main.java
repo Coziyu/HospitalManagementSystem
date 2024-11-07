71ffc6a5f7e3c980e3e4ece1da7a4a62a365759f
Coziyu <59217639+Coziyu@users.noreply.github.com>
2024-11-08 02:38:50 +0800

    remove printing of user.dir

--- src/main/java/org/hms/Main.java
+++ src/main/java/org/hms/Main.java
@@ -4,7 +4,6 @@ package org.hms;
 // Main class to start the application
 public class Main {
     public static void main(String[] args) {
-        System.out.println(System.getProperty("user.dir"));
         App app = new App();
         // Start with the LogInMenu
         app.initialise();
