14618df7387d996f037bca300d1d6dd56c63f361
Tricktail <yeo41155@gmail.com>
2024-11-16 03:01:00 +0800

    JDoc Comments Added for DrugDispensary Service

--- src/main/java/org/hms/services/drugdispensary/DrugInventoryEntry.java
+++ src/main/java/org/hms/services/drugdispensary/DrugInventoryEntry.java
@@ -8,10 +8,21 @@ public class DrugInventoryEntry extends AbstractTableEntry {
     private int quantity;
     private int lowStockAlertThreshold;
 
+    /**
+     * Default constructor initializing DrugInventoryEntry with an invalid ID.
+     */
     public DrugInventoryEntry(){
         super(-1);
     }
 
+    /**
+     * Initializes a DrugInventoryEntry with specified ID, name, quantity, and low stock alert threshold.
+     *
+     * @param id                   the unique ID of the drug entry.
+     * @param name                 the name of the drug.
+     * @param quantity             the current stock quantity of the drug.
+     * @param lowStockAlertThreshold the threshold quantity below which a low stock alert is triggered.
+     */
     public DrugInventoryEntry(int id, String name, int quantity, int lowStockAlertThreshold) {
         super(id);
         this.name = name;
@@ -19,33 +30,64 @@ public class DrugInventoryEntry extends AbstractTableEntry {
         this.lowStockAlertThreshold = lowStockAlertThreshold;
     }
 
+    /**
+     * Retrieves the low stock alert threshold for this drug entry.
+     *
+     * @return the low stock alert threshold.
+     */
     public int getLowStockAlertThreshold() {
         return lowStockAlertThreshold;
     }
 
+    /**
+     * Sets the low stock alert threshold for this drug entry.
+     *
+     * @param lowStockAlertThreshold the new low stock alert threshold to set.
+     */
     public void setLowStockAlertThreshold(int lowStockAlertThreshold) {
         this.lowStockAlertThreshold = lowStockAlertThreshold;
     }
 
+    /**
+     * Retrieves the current stock quantity of the drug.
+     *
+     * @return the stock quantity.
+     */
     public int getQuantity() {
         return quantity;
     }
 
+    /**
+     * Sets the stock quantity for the drug.
+     *
+     * @param quantity the new quantity to set.
+     */
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
 
+    /**
+     * Retrieves the name of the drug.
+     *
+     * @return the drug name.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the drug.
+     *
+     * @param name the new name to set.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
     /**
+     * Converts the drug inventory entry to a CSV formatted string
      * TODO
-     * @return
+     * @return a CSV string representing the entry details
      */
     @Override
     public String toCSVString() {
@@ -55,6 +97,8 @@ public class DrugInventoryEntry extends AbstractTableEntry {
     }
 
     /**
+     * Loads the drug inventory entry details from a CSV formatted string
+     *
      * @param csvLine comma seperated entry values.
      */
     @Override
